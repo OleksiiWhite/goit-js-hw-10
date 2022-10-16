895676873a8d432e40c4f68c18acbae3d31625a3
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-16 19:56:46 +0300

    hw11 fixed

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,33 +1,32 @@
-function createPrewiewMarkup(item) {
-  return `
-                
-                    <li class = 'country'>
-                        <img src = '${item.flags.svg}' />
-                        <p>${item.name}</p>
-                    </li>
-                
-                `;
-}
+// function createPrewiewMarkup({ flags, name }) {
+//   return `<li class="country-list__item">
+//       <img
+//         class="country-list__flag"
+//         src="${flags.svg}"
 
-function createCountryInfoMurkup(item) {
-  `
-                
-                    <div class = 'country'>
-                    
-                        <img src = '${item.flags.svg}' />
-    
-                        <div class = 'country-body'>
-                        
-                            <h3>${item.name}</h3>
-                            <p><b>Region: </b> ${item.region}</p>
-                            <p><b>Capital: </b> ${item.capital}</p>
-                            <p><b>Population: </b> ${item.population.toLocaleString()}</p>
-                            <p><b>Languages: </b> ${item.languages[0].name}</p>
-                        </div>
-    
-                    </div>
-                
-                `;
-}
+//       />
+//       <p class="country-list__name">${name.official}</p>
+//     </li>`;
+// }
 
-export { createPrewiewMarkup, createCountryInfoMurkup };
+// function createCountryInfoMurkup({
+//   flags,
+//   name,
+//   capital,
+//   population,
+//   languages,
+// }) {
+//   const langStr = Object.values(languages).join(', ');
+
+//   return `
+//     <div class="country-list__item">
+//     <img class="country-list__flag"  src="${flags.svg}"></img>
+//     <p class="country-list__name accent">${name.official}</p></div>
+//     <div class="description">
+//     <p class="description__name">Capital: <span class="description__second-name" >${capital}</span><p>
+//     <p class="description__name">Population: <span class="description__second-name">  ${population}</span></p>
+//     <p class="description__name">Languages: <span class="description__second-name" >${langStr}</span></p>
+//     </div>`;
+// }
+
+// export { createPrewiewMarkup, createCountryInfoMurkup };
